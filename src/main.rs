b1cb119e9f1a86213ab74b678f51619c5e70e01d
Chen Liang <chen.liang.cn@outlook.com>
2024-12-05 22:46:20 +0800

    deliver web page with htmx code

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,18 @@ async fn main() {
 }
 
 async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+    Html("<html>
+  <head>
+    <title>HTMX with Axum Backend</title>
+    <script src=\"https://unpkg.com/htmx.org@2.0.3\" integrity=\"sha384-0895/pl2MU10Hqc6jd4RvrthNlDiE9U1tWmX7WRESftEDRosgxNsQG/Ze9YMRzHq\" crossorigin=\"anonymous\"></script>
+
+  </head>
+  <body>
+    <button hx-get=\"http://127.0.0.1:3000/hello\">
+      Hello
+    </button>
+  </body>
+</html>")
 }
 
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
